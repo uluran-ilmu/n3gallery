e6e3893b5cf14da6f57d2fd7bae3dcf0079c5095
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2023-05-28 02:25:22 +0700

    chore: javafx deps

--- src/main/java/N3Gallery/App.java
+++ src/main/java/N3Gallery/App.java
@@ -3,12 +3,25 @@
  */
 package N3Gallery;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vBox = new VBox(new Label("Hello World!"));
+        vBox.setAlignment(Pos.CENTER);
+        primaryStage.setScene(new Scene(vBox, 600, 480));
+        primaryStage.setTitle("Main");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch();
     }
 }
